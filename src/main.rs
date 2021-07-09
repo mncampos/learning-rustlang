2aee70df20006ebbaf6441231758122fe0968933
mncampos <mncampos@inf.ufrgs.br>
2021-07-08 22:49:20 -0300

    guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+
+
 fn main() {
-    println!("Hello, worldd!");
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    println!("Please input your guesses.");
+    loop{
+
+    
+    let mut guess = String::new();
+
+
+    io::stdin().read_line(&mut guess).expect("Failed to load line!");
+
+   
+
+    
+    let guess: u32 = match guess.trim().parse() {
+        Ok(num) => num,
+        Err(_) => { 
+            println!("Please input a number!");
+            continue;
+        }
+    };
+
+    match guess.cmp(&secret_number){
+        Ordering::Less => println!("Too small!"),
+        Ordering::Equal => {
+             println!("You win!");
+             break;
+        }
+        Ordering::Greater => println!("Too big!"),
+    }
+
+    }
+    
 }
